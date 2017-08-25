23894eae5d7415c20f8d943845cd7527e95d160d
Stuart Moore <stuart.moore@gmail.com>
2017-08-25 10:36:45 +0100

    Fix profile.sh

--- src/apt/postfix/profile.sh
+++ src/apt/postfix/profile.sh
@@ -1,5 +1,5 @@
 export PATH="/var/vcap/packages/postfix/apt/usr/bin:/var/vcap/packages/postfix/apt/usr/sbin:$PATH"
-export LD_LIBRARY_PATH="/var/vcap/packages/postfix/apt/usr/lib/postfix:${LD_LIBRARY_PATH:-}"
+export LD_LIBRARY_PATH="/var/vcap/packages/postfix/apt/usr/lib/:${LD_LIBRARY_PATH:-}"
 export INCLUDE_PATH="/var/vcap/packages/postfix/apt/usr/include:${INCLUDE_PATH:-}"
 export CPATH="$INCLUDE_PATH"
 export CPPPATH="$INCLUDE_PATH"
