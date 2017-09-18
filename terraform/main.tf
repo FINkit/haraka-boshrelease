717b70896d6cb08435fc8f724a4c527e5f53d776
Stuart Moore <stuart.moore@gmail.com>
2017-09-18 14:16:16 +0100

    First draft of terraform network stuff

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_firewall" "nat-to-sendgrid" {
+  name    = "${var.prefix}nat-to-sendgrid"
+  network = "${google_compute_network.bosh.name}"
+
+  direction = "EGRESS"
+
+  destination_ranges "159.122.224.196/32, 159.122.224.197/32"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["2525"]
+  }
+
+  target_tags = ["nat"]
+}
