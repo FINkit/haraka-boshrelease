41a924ba196f4bfd742feffc9a3eb78fe95a78b8
Stuart Moore <stuart.moore@gmail.com>
2017-09-18 15:03:10 +0100

    Edits to create-firewall-rule.sh

--- create-firewall-rule.sh
+++ create-firewall-rule.sh
@@ -2,8 +2,8 @@
 # Requires `gcloud version` to be at least 168.0.0
 #gcloud compute firewall-rules create nat-to-sendgrid --target-tags nat --direction egress --allow tcp:2525 --network bosh --destination-ranges 159.122.224.196,159.122.224.197
 
-set +e
-set +x
+set -e
+set -x
 
 zone=$(gcloud config get-value compute/zone 2>/dev/null)
 region=$(gcloud config get-value compute/region 2>/dev/null)
